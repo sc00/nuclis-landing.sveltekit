3240d9cc0cd3696af702b644ba0aa1965f20a54f
sc00 <mail@julian-urabl.com>
2026-05-15 12:06:08 +0200

    [chore] add visual icon component

---
+++ src/lib/components/VisualIcon.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	/**
+	 * TYPES
+	 */
+	interface Props {
+		name: string;
+		label?: string;
+	}
+
+	/**
+	 * PROPS
+	 */
+	let { name, label }: Props = $props();
+</script>
+
+<span
+	class="v-icon"
+	role={label && 'img'}
+	aria-label={label}
+	style="--this-icon: url('/assets/{name}.svg')"
+></span>
+
+<style lang="scss">
+	.v-icon {
+		background-color: currentColor;
+		mask: var(--this-icon) no-repeat center / contain;
+		-webkit-mask: var(--this-icon) no-repeat center / contain;
+	}
+</style>
